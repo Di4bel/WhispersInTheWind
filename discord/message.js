a913f76414ada2ad91de2f4b6e937cb5770565e3
Di4bel <pruefertinte8@gmx.de>
2022-05-27 20:43:23 +0200

    implemented the message.json list with the test commands

--- discord/message.js
+++ discord/message.js
@@ -1,14 +1,24 @@
 require("discord.js");
-const { Client, Discord, Intents } = require('discord.js');
+const { Client, Discord, Intents, Message } = require('discord.js');
+const { parse } = require("dotenv");
 const myIntents = new Intents(32767);
 const client = new Client({ intents: myIntents });
 let discordToken = process.env.DISCORD_TOKEN;
 client.login(discordToken);
 
+// Add a json list
+let list = require("./message.json");
+console.log(list[0][0]);
+console.log(typeof list[0][0]);
+
 // Message listener
 client.on('messageCreate', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+
+    for (let x = 0; x < list.length; x++) {
+        let listitems = list[x];
+        if (msg.content == String(listitems[0])) {
+            msg.reply(String(listitems[1]));
+        }
     }
 }
 );
